b9f09c8ce2f17ce0e042b67e45872d21b0f05f1f
asus <asus@DESKTOP-EHGPA00>
2017-07-07 16:30:28 +0800

    L8

--- Hahow/src/JAVA/L8_index.java
+++ Hahow/src/JAVA/L8_index.java
@@ -1,14 +1,19 @@
 package JAVA;
 
 public class L8_index {
-
+	//和substring一起使用, 透過字元找到索引值在放入substring的參數內
 	public static void main(String[] args) {
-		String str="aaaabb";
+		String str="aabbccdd";
 		// indexOf
 		System.out.println(str.indexOf("a"));
-		
 		//lastIndexOf
 		System.out.println(str.lastIndexOf("b"));
+		//print:bbcc
+		System.out.println(str.substring(str.indexOf("b"),str.lastIndexOf("c")+1));
+		
+		
+		//contains
+		System.out.println(str.contains("z"));
 
 	}
 
